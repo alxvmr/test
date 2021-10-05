bf2fb2460701b46f697164eea7e2f81d972350a6
alxvmr <alexeeva.maria16@gmail.com>
2021-10-05 09:11:58 +0400

    Initial commit

---
+++ practic/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practic
+{
+    class Calc
+    {
+        public double add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double divice(double a, double b)
+        {
+            return a / b;
+        }
+    }
+
+    class Ticket
+    {
+        public bool lucky_ticket(string s)
+        {
+            if (s.Length != 6)
+                throw new Exception("Invalid ticket");
+            return (((s[0] - '0') + (s[1] - '0') + (s[2] - '0')) == ((s[3] - '0') + (s[4] - '0') + (s[5] - '0')));
+        }
+    }
+}
--- practic/UnitTest1.cs
+++ practic/UnitTest1.cs
@@ -1,18 +1,39 @@
 using NUnit.Framework;
+using System;
+using practic;
 
 namespace practic
 {
+    [TestFixture]
+
     public class Tests
     {
+        private Calc calculator;
+        private Ticket ticket;
+
         [SetUp]
         public void Setup()
         {
+            calculator = new Calc();
+            ticket = new Ticket();
         }
 
         [Test]
-        public void Test1()
+        public void Is_lucky_ticket_1()
         {
-            Assert.Pass();
+            Assert.IsTrue(ticket.lucky_ticket("111111"));
+        }
+
+        [Test]
+        public void Is_lucky_ticket_2()
+        {
+            Assert.IsFalse(ticket.lucky_ticket("123456"));
+        }
+
+        [Test]
+        public void Is_lucky_ticket_3()
+        {
+            Assert.Throws<Exception>(() => ticket.lucky_ticket("12345678"));
         }
     }
 }
